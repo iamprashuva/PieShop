fb65a30a8a7e10ff450599c027b40fd7db72ad0a
iamprashuva <prashuvadhital@gmail.com>
2024-04-28 14:53:35 +0545

    create order

---
+++ PieShop/Models/Order.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PieShop.Models
+{
+    public class Order
+    {
+        public int OrderId {  get; set; }
+        public List<OrderDetail>? OrderDetails { get; set; }
+
+        [Required(ErrorMessage ="Enter your first name")]
+        [Display(Name="First name")]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Enter your last name")]
+        [Display(Name = "Last name")]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Enter your address")]
+        [Display(Name = "Address Line 1")]
+        [StringLength(100)]
+        public string AddressLine1 { get; set; } = string.Empty;
+        [Display(Name = "Address Line 2")]
+        public string? AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Please enter your zip code")]
+        [Display(Name ="Zip code")]
+        [StringLength(10, MinimumLength = 4)]
+        public string ZipCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter your city")]
+        [StringLength(50)]
+        public string City { get; set; } = string.Empty;
+        [StringLength(10)]
+        public string? State { get; set; }
+        [Required(ErrorMessage = "Please enter your country")]
+        [StringLength(50)]
+        public string Country { get; set; } = string.Empty;
+
+        [Required(ErrorMessage ="Please enter your phone number")]
+        [StringLength(20)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name ="Phone number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage="The email address is not entered in corect format")]
+
+        public string Email { get; set; }= string.Empty;
+        [BindNever]
+        public decimal OrderTotal { get; set; }
+        [BindNever]
+        public DateTime OrderPlaced { get; set; }
+
+    }
+}
